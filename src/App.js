8f5d186aea1ea9a06e84ba16457c5e2e4d451933
rahul saini <rrahulsaini06@gmail.com>
2024-09-05 15:43:17 +0530

    intialization todo app and shown static list of tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+ const tasks =[{
+  "_id": {
+    "$oid": "66d95d7fc13b991c6be8d41a"
+  },
+  "taskIdea": "manage books",
+  "taskId": 12345,
+  "taskDes": "manage books from others",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "66d95db0c13b991c6be8d41e"
+  },
+  "taskIdea": "buy books",
+  "taskId": 12346,
+  "taskDes": "buy books from market",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "66d95decc13b991c6be8d420"
+  },
+  "taskIdea": "take food",
+  "taskId": 12347,
+  "taskDes": "buy food from market",
+  "__v": 0
+}]
 
 function App() {
+  const rows = [];
+  for (let i = 0; i < tasks.length; i++) {
+      rows.push(( <div id ="social_media" class="box">
+        <h3 >{tasks[i].taskIdea}</h3>
+       <p>{tasks[i].taskDes}</p>
+    </div>));
+    console.log("ROWS ARREY",rows)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main-div">
+    <div class="side-bar">
+        <div class="menu">
+            <h3><strong>menu</strong></h3>
+            <input class="Search" type="text" placeholder="Search.."/>
+            <div>
+                <h4 class="tasks"><strong>tasks</strong></h4>
+                <a class="upcoming">upcoming</a>
+                <a class="today">Today</a>
+                <a class="calender">calender</a>
+                <a class="stickyA">Sticky Wall</a>
+            </div>
+            <div>
+                <h4><strong>Lists</strong></h4>
+                <a>personal</a>
+                <a>work</a>
+                <a>list </a>
+                <a>add new list</a>
+            </div>
+            <div>
+                <h4><strong>Tags</strong></h4>
+                <a>Tag 1</a>
+                <a>Tag 2</a>
+                <a>add tag</a>
+              
+            </div>
+            <a>Settings</a>
+            <a>Sign out</a>
+        </div>
+    </div>    
+    <div class="container">
+        <h2> sticky wall</h2>
+        {rows}
+     </div>
     </div>
   );
 }
